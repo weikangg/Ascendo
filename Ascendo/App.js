9e85fde6a96b7d5ec43a6598c3da24cfeb45edba
Kane <kanetan4@gmail.com>
2023-06-17 03:02:50 +0800

    Update App.js

--- Ascendo/App.js
+++ Ascendo/App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import * as React from 'react';
+import MainContainer from './navigation/MainContainer';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
+    <>
       <StatusBar style="auto" />
-    </View>
+      <MainContainer />
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+// export default function App() {
+//   return (
+//     <MainContainer/>
+//     // <View style={styles.container}>
+//     //   <Text>Hello World!</Text>
+//     //   <StatusBar style="auto" />
+//     // </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
