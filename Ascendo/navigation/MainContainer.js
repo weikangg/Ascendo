00f076d1751c14940f75319e9e583a0b96c356a5
Yuandjom <ziyangjohn@gmail.com>
2023-06-17 19:05:56 +0800

    stack screen to navigate to profile

--- Ascendo/navigation/MainContainer.js
+++ Ascendo/navigation/MainContainer.js
@@ -3,6 +3,8 @@ import { View, Text, Image } from "react-native";
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createStackNavigator } from "@react-navigation/stack";
+
 import Ionicons from "react-native-vector-icons/Ionicons";
 
 import HomeScreen from "./screens/HomeScreen";
@@ -10,6 +12,7 @@ import CommunityScreen from "./screens/CommunityScreen";
 import GamesScreen from "./screens/GamesScreen";
 import RewardsScreen from "./screens/RewardsScreen";
 import TasksScreen from "./screens/TasksScreen";
+import ProfileScreen from "./screens/ProfileScreen";
 
 const homeName = "Home";
 const communityName = "Community";
@@ -18,82 +21,93 @@ const rewardsName = "Rewards";
 const tasksName = "Tasks";
 
 const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
 
 export default function MainContainer() {
   return (
     <NavigationContainer>
-      <Tab.Navigator
-        initialRouteName={homeName}
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
-            let rn = route.name;
-
-            if (rn === homeName) {
-              iconName = focused ? "home" : "home-outline";
-            } else if (rn === communityName) {
-              iconName = focused ? "people" : "people-outline";
-            } else if (rn === gamesName) {
-              iconName = focused
-                ? "game-controller"
-                : "game-controller-outline";
-            } else if (rn === rewardsName) {
-              iconName = focused ? "card" : "card-outline";
-            } else if (rn === tasksName) {
-              iconName = focused ? "book" : "book-outline";
-            }
-            return <Ionicons name={iconName} size={size} color={color} />;
-          },
-          // screenOptions: {
-          tabBarActiveTintColor: "black",
-          tabBarInactiveTintColor: "black",
-          tabBarLabelStyle: {
-            paddingBottom: 10,
-            fontSize: 11,
-          },
-          tabBarStyle: {
-            backgroundColor: "#469FD1",
-            paddingTop: 10,
-            height: 90,
-          },
-        })}
-        // tabBarOptions={{
-        //   activeTintColor: "black",
-        //   inactiveTintColor: "black",
-        //   labelStyle: { paddingBottom: 0, fontSize: 11 },
-        // }}
-      >
-        <Tab.Screen
-          name={rewardsName}
-          component={RewardsScreen}
-          options={{
-            headerTitle: "Rewards",
-            headerTitleAlign: "center",
-            headerTitleStyle: {
-              fontWeight: "bold", // Adjust this as needed
-              paddingBottom: 20,
-            },
-            headerRight: () => (
-              <View
-                style={{
-                  flexDirection: "row",
-                  alignItems: "center",
-                  marginRight: 20,
-                }}
-              >
-                <Image
-                  style={{ width: 30, height: 30 }} // Adjust size as necessary
-                  source={require("../assets/rewards_page/profile-icon.png")} // profile icon source
-                />
-              </View>
-            ),
-          }}
+      <Stack.Navigator>
+        <Stack.Screen
+          name="TabNavigator"
+          component={TabNavigatorScreen}
+          options={{ headerShown: false }}
         />
-        <Tab.Screen name={tasksName} component={TasksScreen} />
-        <Tab.Screen name={homeName} component={HomeScreen} />
-        <Tab.Screen name={gamesName} component={GamesScreen} />
-        <Tab.Screen name={communityName} component={CommunityScreen} />
-      </Tab.Navigator>
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
+const TabNavigatorScreen = () => {
+  return (
+    <Tab.Navigator
+      initialRouteName={homeName}
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          let rn = route.name;
+
+          if (rn === homeName) {
+            iconName = focused ? "home" : "home-outline";
+          } else if (rn === communityName) {
+            iconName = focused ? "people" : "people-outline";
+          } else if (rn === gamesName) {
+            iconName = focused ? "game-controller" : "game-controller-outline";
+          } else if (rn === rewardsName) {
+            iconName = focused ? "card" : "card-outline";
+          } else if (rn === tasksName) {
+            iconName = focused ? "book" : "book-outline";
+          }
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        // screenOptions: {
+        tabBarActiveTintColor: "black",
+        tabBarInactiveTintColor: "black",
+        tabBarLabelStyle: {
+          paddingBottom: 10,
+          fontSize: 11,
+        },
+        tabBarStyle: {
+          backgroundColor: "#469FD1",
+          paddingTop: 10,
+          height: 90,
+        },
+      })}
+      // tabBarOptions={{
+      //   activeTintColor: "black",
+      //   inactiveTintColor: "black",
+      //   labelStyle: { paddingBottom: 0, fontSize: 11 },
+      // }}
+    >
+      <Tab.Screen
+        name={rewardsName}
+        component={RewardsScreen}
+        options={{
+          headerTitle: "Rewards",
+          headerTitleAlign: "center",
+          headerTitleStyle: {
+            fontWeight: "bold", // Adjust this as needed
+            paddingBottom: 20,
+          },
+          headerRight: () => (
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                marginRight: 20,
+              }}
+            >
+              <Image
+                style={{ width: 30, height: 30 }} // Adjust size as necessary
+                source={require("../assets/rewards_page/profile-icon.png")} // profile icon source
+              />
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen name={tasksName} component={TasksScreen} />
+      <Tab.Screen name={homeName} component={HomeScreen} />
+      <Tab.Screen name={gamesName} component={GamesScreen} />
+      <Tab.Screen name={communityName} component={CommunityScreen} />
+    </Tab.Navigator>
+  );
+};
---
+++ Ascendo/navigation/screens/ProfileScreen.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+import { Text, View } from "react-native";
+
+export default function ProfileScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text
+        onPress={() => navigation.navigate("Home")}
+        style={{ fontSize: 26, fontWeight: "bold" }}
+      >
+        Games
+      </Text>
+    </View>
+  );
+}
