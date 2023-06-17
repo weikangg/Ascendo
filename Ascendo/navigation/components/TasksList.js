1ce7edda185510d734dafc3a6b9200d41935d2dd
kenfyxx <kenfongyexuan@gmail.com>
2023-06-17 16:40:08 +0800

    modified task screen and added task lists

---
+++ Ascendo/navigation/components/TasksList.js
@@ -0,0 +1,302 @@
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  FlatList,
+  StyleSheet,
+  Dimensions,
+  TouchableOpacity,
+} from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
+
+const TasksList = () => {
+  const [completedTasks, setCompletedTasks] = useState([]);
+  const [selectedTab, setSelectedTab] = useState("current");
+
+  const toggleTaskCompletion = (taskId) => {
+    if (completedTasks.includes(taskId)) {
+      setCompletedTasks(completedTasks.filter((id) => id !== taskId));
+    } else {
+      setCompletedTasks([...completedTasks, taskId]);
+    }
+  };
+
+  // Dummy task data for testing
+  const tasks = [
+    {
+      id: 1,
+      title: "Task 1",
+      duration: "3h 30min",
+      document: "Main Website",
+      details:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eu...",
+    },
+    {
+      id: 2,
+      title: "Task 2",
+      duration: "2h 30min",
+      document: "Main Website",
+      details:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eu...",
+    },
+    {
+      id: 3,
+      title: "Task 3",
+      duration: "8h 30min",
+      document: "Main Website",
+      details:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eu...",
+    },
+    {
+      id: 4,
+      title: "Task 4",
+      duration: "1h 30min",
+      document: "Contact Us",
+      details:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eu...",
+    },
+  ];
+
+  const addToCalendar = () => {
+    // Logic to open the user's calendar app
+    console.log("Add to Calendar pressed");
+  };
+
+  const navigateToHistoryTasks = () => {
+    // Logic to navigate to the history tasks page
+    console.log("Navigating to History Tasks page");
+  };
+
+  return (
+    <View style={styles.container}>
+      {/* <View style={styles.headerContainer}>
+        <View style={styles.headerRectangle}>
+          <Text style={styles.currentTasksText}>Current Tasks</Text>
+          <Text style={styles.historyText}>History</Text>
+        </View>
+      </View> */}
+      <View style={styles.headerContainer}>
+        <TouchableOpacity
+          style={[
+            styles.tabButton,
+            selectedTab === "current" && styles.selectedTabButton,
+          ]}
+          onPress={() => setSelectedTab("current")}
+        >
+          <Text
+            style={[
+              styles.tabButtonText,
+              selectedTab === "current" && styles.selectedTabButtonText,
+            ]}
+          >
+            Current Tasks
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[
+            styles.tabButton,
+            selectedTab === "history" && styles.selectedTabButton,
+          ]}
+          onPress={navigateToHistoryTasks}
+        >
+          <Text
+            style={[
+              styles.tabButtonText,
+              selectedTab === "history" && styles.selectedTabButtonText,
+            ]}
+          >
+            History
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <FlatList
+        data={tasks}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View
+            style={[
+              styles.taskContainer,
+              completedTasks.includes(item.id) && styles.completedTaskContainer,
+            ]}
+          >
+            <View style={styles.taskHeader}>
+              <Text
+                style={[
+                  styles.title,
+                  completedTasks.includes(item.id) && styles.completedTitle,
+                ]}
+              >
+                {item.title}
+              </Text>
+              <TouchableOpacity
+                onPress={() => toggleTaskCompletion(item.id)}
+                style={[
+                  styles.toggleButton,
+                  completedTasks.includes(item.id) && styles.completedButton,
+                ]}
+              >
+                <FontAwesome
+                  name={
+                    completedTasks.includes(item.id)
+                      ? "check-circle"
+                      : "circle-thin"
+                  }
+                  style={styles.toggleIcon}
+                />
+              </TouchableOpacity>
+            </View>
+            <View style={styles.row}>
+              <FontAwesome name="clock-o" style={styles.icon} />
+              <Text style={styles.duration}>{item.duration}</Text>
+              <View style={styles.documentContainer}>
+                <FontAwesome name="file-text-o" style={styles.documentIcon} />
+                <Text style={styles.documentInfo}>{item.document}</Text>
+              </View>
+            </View>
+            <Text style={styles.details}>{item.details}</Text>
+            <View style={styles.calendarContainer}>
+              <FontAwesome name="calendar" style={styles.calendarIcon} />
+              <TouchableOpacity
+                onPress={addToCalendar}
+                style={styles.addToCalendarButton}
+              >
+                <Text style={styles.addToCalendarButtonText}>
+                  Add to Calendar
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        )}
+        contentContainerStyle={styles.contentContainer}
+      />
+    </View>
+  );
+};
+
+const windowWidth = Dimensions.get("window").width;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+  headerContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    marginBottom: 16,
+    width: "100%",
+  },
+  tabButton: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingVertical: 8,
+    borderBottomWidth: 2,
+    borderColor: "transparent",
+  },
+  selectedTabButton: {
+    borderColor: "#469FD1",
+  },
+  tabButtonText: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  selectedTabButtonText: {
+    fontWeight: "bold",
+  },
+  contentContainer: {
+    flexGrow: 1,
+  },
+  taskContainer: {
+    backgroundColor: "#e0e0e0",
+    borderRadius: 8,
+    padding: 16,
+    marginBottom: 16,
+    width: "100%", // Take up full width within content container
+    maxWidth: 400, // Set maximum width to prevent it from being too wide
+  },
+  completedTaskContainer: {
+    opacity: 0.6,
+  },
+  taskHeader: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 8,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 8,
+  },
+  completedTitle: {
+    textDecorationLine: "line-through",
+  },
+  toggleButton: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+    borderWidth: 1,
+    borderColor: "black",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  completedButton: {
+    backgroundColor: "green",
+    borderColor: "green",
+  },
+  toggleIcon: {
+    fontSize: 20,
+    color: "black",
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 8,
+  },
+  icon: {
+    fontSize: 16,
+    marginRight: 8,
+  },
+  duration: {
+    fontSize: 16,
+  },
+  documentContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginLeft: 8,
+  },
+  documentIcon: {
+    fontSize: 16,
+    marginRight: 4,
+  },
+  documentInfo: {
+    fontSize: 16,
+  },
+  details: {
+    fontSize: 14,
+  },
+  calendarContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 8,
+  },
+  calendarIcon: {
+    fontSize: 16,
+    marginRight: 4,
+  },
+  addToCalendarButton: {
+    backgroundColor: "#469FD1",
+    borderRadius: 4,
+    paddingVertical: 8,
+    paddingHorizontal: 12,
+    marginLeft: 8,
+  },
+  addToCalendarButtonText: {
+    color: "white",
+    fontSize: 14,
+  },
+});
+
+export default TasksList;
--- Ascendo/navigation/screens/TasksScreen.js
+++ Ascendo/navigation/screens/TasksScreen.js
@@ -1,12 +1,38 @@
-import * as React from 'react';
-import {Text, View } from "react-native";
+import * as React from "react";
+import { Text, View, StyleSheet, FlatList } from "react-native";
+import TasksList from "../components/TasksList";
 
-export default function TasksScreen({navigation}) {
-    return (
-        <View style={{flex:1, alignItems:'center',justifyContent:'center'}}>
-            <Text
-                onPress={() => navigation.navigate('Home')}
-                style = {{fontSize: 26, fontWeight: 'bold'}}>Tasks</Text>
-        </View>
-    )
+// const TasksListScreen = () => {
+//   return (
+//     <View style={styles.container}>
+//       <TasksList />
+//     </View>
+//   );
+// };
+
+// export default TasksScreen;
+
+export default function TasksScreen({ navigation }) {
+  return (
+    // <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+    //   <Text
+    //     onPress={() => navigation.navigate("Home")}
+    //     style={{ fontSize: 26, fontWeight: "bold" }}
+    //   >
+    //     Tasks
+    //   </Text>
+    // </View>
+    <View style={styles.container}>
+      <TasksList />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 16,
+  },
+});
