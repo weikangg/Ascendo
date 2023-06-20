a40862d8ca57e31322da89282fd0f04209f63350
Kane <kanetan4@gmail.com>
2023-06-20 21:32:24 +0800

    AWS Authentication

---
+++ navigation/components/SignOutButton.js
@@ -0,0 +1,31 @@
+import { withAuthenticator, useAuthenticator } from '@aws-amplify/ui-react-native';
+import { Pressable, Text, StyleSheet } from "react-native";
+
+// retrieves only the current value of 'user' from 'useAuthenticator'
+const userSelector = (context) => [context.user]
+
+const SignOutButton = () => {
+  const { user, signOut } = useAuthenticator(userSelector);
+  return (
+    <Pressable onPress={signOut} style={styles.buttonContainer}>
+      <Text style={styles.buttonText}>Hello, {user.username}! Click here to sign out!</Text>
+    </Pressable>
+  )
+};
+
+const styles = StyleSheet.create({
+    buttonContainer: {
+      backgroundColor: "#0386D0",
+      padding: 10,
+      borderRadius: 5,
+      alignItems: "center",
+      marginTop: 10,
+    },
+    buttonText: {
+      color: "white",
+      fontSize: 16,
+      fontWeight: "bold",
+    },
+  });
+  
+  export default SignOutButton;
