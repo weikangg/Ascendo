64e49685df28ee97f35a5453e393b4d8d36b9b93
kenfyxx <kenfongyexuan@gmail.com>
2023-06-21 16:51:42 +0800

    swipe delete

--- navigation/components/TasksList.js
+++ navigation/components/TasksList.js
@@ -13,6 +13,7 @@ import {
 } from "react-native";
 import { FontAwesome, Feather } from "@expo/vector-icons";
 import Picture from "../../assets/rewards_page/ascendo_logo.png";
+import { SwipeListView } from "react-native-swipe-list-view";
 
 const TasksList = ({ navigation }) => {
   const [completedTasks, setCompletedTasks] = useState([]);
@@ -65,7 +66,27 @@ const TasksList = ({ navigation }) => {
     }
   };
 
-  const TaskItem = ({ item }) => {
+  const deleteTask = async (taskId) => {
+    try {
+      // Send a DELETE request to your API to delete the task with the specified ID
+      await fetch(
+        `https://iwbybrwtpe.execute-api.ap-southeast-1.amazonaws.com/tasks/${taskId}`,
+        {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      // Update the tasks state by removing the deleted task
+      setTasks(tasks.filter((task) => task.id !== taskId));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const renderTaskItem = ({ item }) => {
     const isExpanded = item.id === expandedTaskId;
 
     return (
@@ -143,12 +164,23 @@ const TasksList = ({ navigation }) => {
     );
   };
 
+  const renderSwipeableItem = ({ item }) => (
+    <View style={styles.rowBack}>
+      <TouchableOpacity
+        style={styles.deleteButton}
+        onPress={() => deleteTask(item.id)}
+      >
+        <FontAwesome name="trash" style={styles.deleteIcon} />
+      </TouchableOpacity>
+    </View>
+  );
+
   return (
     <View style={styles.container}>
-      <FlatList
+      <SwipeListView
         data={tasks}
         keyExtractor={(item) => item.id.toString()}
-        renderItem={({ item }) => <TaskItem item={item} />}
+        renderItem={renderTaskItem}
         contentContainerStyle={styles.contentContainer}
         refreshControl={
           <RefreshControl
@@ -157,6 +189,9 @@ const TasksList = ({ navigation }) => {
             colors={["#469FD1"]}
           />
         }
+        renderHiddenItem={renderSwipeableItem}
+        rightOpenValue={-80} // Width of the delete button
+        disableRightSwipe={true} // Disable right swipe on list items
       />
     </View>
   );
@@ -291,6 +326,31 @@ const styles = StyleSheet.create({
     marginTop: 8,
     marginBottom: 8,
     borderRadius: 8,
+  },
+  rowBack: {
+    alignItems: "center",
+    backgroundColor: "#ff0000",
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    paddingRight: 15,
+    borderRadius: 8,
+    padding: 20,
+    marginBottom: 16,
+    marginLeft: 15,
+    width: "95%",
+  },
+  deleteButton: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: 80,
+    borderRadius: 8,
+    padding: 20,
+    marginBottom: 16,
+  },
+  deleteIcon: {
+    color: "#ffffff",
+    fontSize: 24,
   },
 });
 
