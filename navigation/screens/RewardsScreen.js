9810dde4554a8f9ec3247fcf2b00dc2d58d416fa
Yuandjom <ziyangjohn@gmail.com>
2023-06-22 00:47:38 +0800

    Revert "Added Capsule Station (#9)"
    This reverts commit f6b47935fe67f39288431649b764c7178b19bc3b.

--- navigation/screens/RewardsScreen.js
+++ navigation/screens/RewardsScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, {useState, useEffect} from "react";
 import {
     ScrollView,
     View,
@@ -45,7 +45,6 @@ export default function RewardsScreen({ navigation }) {
 
         fetchRewards();
     }, []);
-    console.log(rewards);
 
     const renderHeader = () => (
         <View style={styles.headerContainer}>
@@ -166,6 +165,7 @@ const styles = StyleSheet.create({
     qrCodeImage: {
         width: "100%",
         height: "100%",
+
     },
     qrCodeText: {
         fontFamily: "System",
