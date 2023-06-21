2ceb4c6438b64768f08d410570fb49fa9fb5dae0
Soqoro <qirong36@gmail.com>
2023-06-21 10:43:59 +0800

    Custom UI with Auth

--- navigation/components/SignOutButton.js
+++ navigation/components/SignOutButton.js
@@ -1,39 +1,40 @@
-import {
-  withAuthenticator,
-  useAuthenticator,
+{
+    /*import {
+    withAuthenticator,
+    useAuthenticator,
 } from "@aws-amplify/ui-react-native";
 import { Pressable, Text, StyleSheet } from "react-native";
 
-// retrieves only the current value of 'user' from 'useAuthenticator'
+ retrieves only the current value of 'user' from 'useAuthenticator'
 const userSelector = (context) => [context.user];
 
-const SignOutButton = () => {
-  const { user, signOut } = useAuthenticator(userSelector);
-  return (
-    <Pressable onPress={signOut} style={styles.buttonContainer}>
-      <Text style={styles.buttonText}>◀ Sign Out</Text>
-    </Pressable>
-  );
+{
+    /*const SignOutButton = () => {
+    const { user, signOut } = useAuthenticator(userSelector);
+    return (
+        <Pressable style={styles.buttonContainer}>
+            <Text style={styles.buttonText}>
+                Hello, {user.username}! Click here to sign out!
+            </Text>
+        </Pressable>
+    );
 };
+}
 
 const styles = StyleSheet.create({
-  buttonContainer: {
-    position: "absolute",
-    backgroundColor: "white",
-    padding: 10,
-    borderRadius: 5,
-    alignItems: "center",
-    marginTop: -10,
-    marginLeft: -40,
-    top: 0,
-    left: 0,
-    zIndex: 9,
-  },
-  buttonText: {
-    color: "black",
-    fontSize: 12,
-    fontWeight: "bold",
-  },
-});
+    buttonContainer: {
+        backgroundColor: "#0386D0",
+        padding: 10,
+        borderRadius: 5,
+        alignItems: "center",
+        marginTop: 10,
+    },
+    buttonText: {
+        color: "white",
+        fontSize: 16,
+        fontWeight: "bold",
+    },
+});*/
+}
 
-export default SignOutButton;
+//export default SignOutButton;
